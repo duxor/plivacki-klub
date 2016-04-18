db2831c5fffd8dd6c3f6f3b5769987e116f1d196
AJovic <ajovic990@gmail.com>
2016-04-18 18:53:42 +0200

    AJ 0.0.0

--- config/mail.php
+++ config/mail.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+    'host' => env('MAIL_HOST', 'smtp.gmail.com'),
 
     /*
     |--------------------------------------------------------------------------
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'ajovic990@gmail.com', 'name' => 'Jovic zmaj'],
 
     /*
     |--------------------------------------------------------------------------
