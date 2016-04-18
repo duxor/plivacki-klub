05626dd5878cfbc02d48ac70b56c910761ef0b85
AJovic <ajovic990@gmail.com>
2016-04-18 19:38:25 +0200

    AJ 0.0.0

--- resources/views/auth/emails/password.blade.php
+++ resources/views/auth/emails/password.blade.php
@@ -1,1 +1,1 @@
-Click here to reset your password: <a href="{{ $link = url('password/reset', $token).'?email='.urlencode($user->getEmailForPasswordReset()) }}"> {{ $link }} </a>
+Za resetovanje šifre kliknite na sledeći link: <a href="{{ $link = url('password/reset', $token).'?email='.urlencode($user->getEmailForPasswordReset()) }}"> {{ $link }} </a>
