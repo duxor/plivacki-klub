d8d7736757652d2819d0facb54ef0c0575e9e861
Saša Jović <jovicsasajovic@gmail.com>
2016-04-19 21:51:17 +0200

    0.13

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -19,11 +19,12 @@
         .jssora02r:hover { background-position: -183px -33px; }
         .jssora02l.jssora02ldn { background-position: -3px -33px; }
         .jssora02r.jssora02rdn { background-position: -63px -33px; }
-        .jssort11 .p {    position: absolute;    top: 0;    left: 0;    width: 300px;    height: 100px;    background: #181818;}.jssort11 .tp {    position: absolute;    top: 0;    left: 0;    width: 100%;    height: 100%;    border: none;}.jssort11 .i, .jssort11 .pav:hover .i {    position: absolute;    top: 3px;    left: 3px;    width: 80px;    height: 50px;    border: white 1px dashed;}* html .jssort11 .i {    width /**/: 62px;    height /**/: 32px;}.jssort11 .pav .i {    border: white 1px solid;}.jssort11 .t, .jssort11 .pav:hover .t {    position: absolute;    top: 3px;    left: 68px;    width: 129px;    height: 32px;    line-height: 32px;    text-align: center;    color: #fc9835;    font-size: 13px;    font-weight: 700;}.jssort11 .pav .t, .jssort11 .p:hover .t {    color: #fff;}.jssort11 .c, .jssort11 .pav:hover .c {    position: absolute;    top: 48px;    left: 3px;    width: 197px;    height: 31px;    line-height: 31px;    color: #fff;    font-size: 14px;    font-weight: 400;    overflow: hidden;}.jssort11 .pav .c, .jssort11 .p:hover .c {    color: #fc9835;}.jssort11 .t, .jssort11 .c {    transition: color 2s;    -moz-transition: color 2s;    -webkit-transition: color 2s;    -o-transition: color 2s;}.jssort11 .p:hover .t, .jssort11 .pav:hover .t, .jssort11 .p:hover .c, .jssort11 .pav:hover .c {    transition: none;    -moz-transition: none;    -webkit-transition: none;    -o-transition: none;}.jssort11 .p:hover, .jssort11 .pav:hover {    background: #333;}.jssort11 .pav, .jssort11 .p.pdn {    background: #462300;}
+        .jssort11 .p {    position: absolute;    top: 0;    left: 0;    width: 300px;    height: 100px;    background: #003748;}.jssort11 .tp {    position: absolute;    top: 0;    left: 0;    width: 100%;    height: 100%;    border: none;}.jssort11 .i, .jssort11 .pav:hover .i {    position: absolute;    top: 3px;    left: 3px;    width: 80px;    height: 50px;    border: white 1px dashed;}* html .jssort11 .i {    width /**/: 62px;    height /**/: 32px;}.jssort11 .pav .i {    border: white 1px solid;}.jssort11 .t, .jssort11 .pav:hover .t {    position: absolute;    top: 3px;    left: 68px;    width: 129px;    height: 32px;    line-height: 32px;    text-align: center;    color: #fc9835;    font-size: 13px;    font-weight: 700;}.jssort11 .pav .t, .jssort11 .p:hover .t {    color: #fff;}.jssort11 .c, .jssort11 .pav:hover .c {    position: absolute;    top: 48px;    left: 3px;    width: 197px;    height: 31px;    line-height: 31px;    color: #fff;    font-size: 14px;    font-weight: 400;    overflow: hidden;}.jssort11 .pav .c, .jssort11 .p:hover .c {    color: white;}.jssort11 .t, .jssort11 .c {    transition: color 2s;    -moz-transition: color 2s;    -webkit-transition: color 2s;    -o-transition: color 2s;}.jssort11 .p:hover .t, .jssort11 .pav:hover .t, .jssort11 .p:hover .c, .jssort11 .pav:hover .c {    transition: none;    -moz-transition: none;    -webkit-transition: none;    -o-transition: none;}.jssort11 .p:hover, .jssort11 .pav:hover {    background: #333;}.jssort11 .pav, .jssort11 .p.pdn {    background: #462300;}
 
     </style>
  {{-- SLIDER START--}}
-    <div id="jssor_1" style="position: relative; margin: 0 auto; top: 0px; left: 0px; width: 1510px; height: 400px; overflow: hidden; visibility: hidden; background-color: #000000;">
+    <div class="container-fluid" >
+    <div id="jssor_1" style="position: relative; margin:0 auto; top: 0px; left: 0px; width: 1510px; height: 400px; overflow: hidden; visibility: hidden; background-color: #000000;">
         <!-- Loading Screen -->
         <div data-u="loading" style="position: absolute; top: 0px; left: 0px;">
             <div style="filter: alpha(opacity=70); opacity: 0.7; position: absolute; display: block; top: 0px; left: 0px; width: 100%; height: 100%;"></div>
@@ -32,6 +33,10 @@
         <div data-u="slides" style="cursor: default; position: relative; top: 0px; left: 0px; width: 1200px; height: 400px; overflow: hidden;">
             <div data-p="112.50" style="display: none;">
                 <img data-u="image" src="img/002.jpg" />
+                <div style="position: absolute; margin-right: 0px;color: #ffffff; width: 100%; height: 20%; background-color: #080808;bottom:0; left: 0; opacity: 0.6; "">
+                    <a style="color: #ffffff;  font-size: 18px; text-decoration: none;" href="#">vesti vesti asdfasdf asdf asdf asd fasdfasdfasdfasdfasdd
+                    asdfasdf asdf asdf asdf asdf asdf asdf</a>
+                </div>
                 <div data-u="thumb">
                     <img class="i" src="img/thumb-002.jpg" />
                     <div class="t">Banner Rotator</div>
@@ -40,6 +45,10 @@
             </div>
             <div data-p="112.50" style="display: none;">
                 <img data-u="image" src="img/003.jpg" />
+                <div style="position: absolute; margin-right: 0px;color: #ffffff; width: 100%; height: 20%; background-color: #080808;bottom:0; left: 0; opacity: 0.6; "">
+                <a style="color: #ffffff;  font-size: 18px; text-decoration: none;" href="#">vesti vesti asdfasdf asdf asdf asd fasdfasdfasdfasdfasdd
+                    asdfasdf asdf asdf asdf asdf asdf asdf</a>
+            </div>
                 <div data-u="thumb">
                     <img class="i" src="img/thumb-003.jpg" />
                     <div class="t">Image Gallery</div>
@@ -48,6 +57,10 @@
             </div>
             <div data-p="112.50" style="display: none;">
                 <img data-u="image" src="img/004.jpg" />
+                <div style="position: absolute; margin-right: 0px;color: #ffffff; width: 100%; height: 20%; background-color: #080808;bottom:0; left: 0; opacity: 0.6; "">
+                <a style="color: #ffffff;  font-size: 18px; text-decoration: none;" href="#">vesti vesti asdfasdf asdf asdf asd fasdfasdfasdfasdfasdd
+                    asdfasdf asdf asdf asdf asdf asdf asdf</a>
+            </div>
                 <div data-u="thumb">
                     <img class="i" src="img/thumb-004.jpg" />
                     <div class="t">Carousel</div>
@@ -56,6 +69,10 @@
             </div>
             <div data-p="112.50" style="display: none;">
                 <img data-u="image" src="img/005.jpg" />
+                <div style="position: absolute; margin-right: 0px;color: #ffffff; width: 100%; height: 20%; background-color: #080808;bottom:0; left: 0; opacity: 0.6; "">
+                <a style="color: #ffffff;  font-size: 18px; text-decoration: none;" href="#">vesti vesti asdfasdf asdf asdf asd fasdfasdfasdfasdfasdd
+                    asdfasdf asdf asdf asdf asdf asdf asdf</a>
+            </div>
                 <div data-u="thumb">
                     <img class="i" src="img/thumb-005.jpg" />
                     <div class="t">Themes</div>
@@ -64,6 +81,10 @@
             </div>
             <div data-p="112.50" style="display: none;">
                 <img data-u="image" src="img/006.jpg" />
+                <div style="position: absolute; margin-right: 0px;color: #ffffff; width: 100%; height: 20%; background-color: #080808;bottom:0; left: 0; opacity: 0.6; "">
+                <a style="color: #ffffff;  font-size: 18px; text-decoration: none;" href="#">vesti vesti asdfasdf asdf asdf asd fasdfasdfasdfasdfasdd
+                    asdfasdf asdf asdf asdf asdf asdf asdf</a>
+            </div>
                 <div data-u="thumb">
                     <img class="i" src="img/thumb-006.jpg" />
                     <div class="t">Tab Slider</div>
@@ -72,6 +93,10 @@
             </div>
             <div data-p="112.50" style="display: none;">
                 <img data-u="image" src="img/006.jpg" />
+                <div style="position: absolute; margin-right: 0px;color: #ffffff; width: 100%; height: 20%; background-color: #080808;bottom:0; left: 0; opacity: 0.6; "">
+                <a style="color: #ffffff;  font-size: 18px; text-decoration: none;" href="#">vesti vesti asdfasdf asdf asdf asd fasdfasdfasdfasdfasdd
+                    asdfasdf asdf asdf asdf asdf asdf asdf</a>
+            </div>
                 <div data-u="thumb">
                     <img class="i" src="img/thumb-006.jpg" />
                     <div class="t">Tab Slider</div>
@@ -94,6 +119,7 @@
         <!-- Arrow Navigator -->
         <span data-u="arrowleft" class="jssora02l" style="top:0px;left:8px;width:55px;height:55px;" data-autocenter="2"></span>
         <span data-u="arrowright" class="jssora02r" style="top:0px;right:418px;width:55px;height:55px;" data-autocenter="2"></span>
+    </div>
     </div>
     {{-- SLIDER END--}}
     <br/>
@@ -169,6 +195,27 @@
                 </p>
             </div>
         </div>
+        <div style="text-align: center" class="row">
+            <nav >
+                <ul class="pagination">
+                    <li>
+                        <a href="#" aria-label="Previous">
+                            <span aria-hidden="true">&laquo;</span>
+                        </a>
+                    </li>
+                    <li><a href="#">1</a></li>
+                    <li><a href="#">2</a></li>
+                    <li><a href="#">3</a></li>
+                    <li><a href="#">4</a></li>
+                    <li><a href="#">5</a></li>
+                    <li>
+                        <a href="#" aria-label="Next">
+                            <span aria-hidden="true">&raquo;</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     </div>
 
     {{-- OBAVEŠTENJE END--}}
@@ -192,4 +239,41 @@
         </div>
     </div>{{-- SPONZORI END--}}
 </div>
+    <footer style="background-image: url('img/footer.png')" >
+      <br/><br/>
+        <div class="row">
+            <div class="col-md-2">
+                <ul class="footer_link" style="list-style-type: none">
+                    <li class="active1"><a href="#">Početna </a></li>
+                    <li ><a href="#">O nama</a></li>
+                    <li><a href="#">Vizija</a></li>
+                    <li><a href="#">Takmičari</a></li>
+                    <li><a href="#">Rekordi</a></li>
+                </ul>
+            </div>
+            <div class="col-md-2">
+                <ul class="footer_link">
+                    <li><a href="#">Kalendar</a></li>
+                    <li><a href="#">Rezultati</a></li>
+                    <li><a href="#">Galerija</a></li>
+                    <li><a href="#">Norme</a></li>
+                </ul>
+            </div>
+            <div style="color: #ffffff; margin-left: 40px;" class="col-md-2">
+                <div>Klub dubočica</div>
+                <div>Stojana LJuvića 23</div>
+                <div>Leskovac</div><br/>
+                <div>Pera Kojot</div>
+                <div>066/555/666/</div>
+                <div>email: asdf@gmail.com</div>
+            </div>
+            <div class="col-md-2">
+                <ul class="nav navbar-nav navbar-right" >
+                        <li style="display: inline;" ><a href="#"><img class="twiter" src="img/twitter.png"></a></li>
+                    <li style="display: inline;"><a href="#"><img class="face" src="img/facebook.png"></a></li>
+                    <li style="display: inline;"><a href="#"><img class="link" src="img/linkedin.png"></a></li>
+                </ul>
+            </div>
+        </div><br/><br/>
+    </footer>
 @endsection
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -12,49 +12,56 @@
 </head>
 
 <body>
-    <nav style="margin-bottom: 0px;" class="navbar navbar-default">
-        <ul class="nav navbar-nav">
-            <li ><a class="navbar-brand" href="/"><img  id="logo" src="img/logo.png"/></a></li>
-            <li><a id="sitemap" href="">Sitemap</a></li>
-        </ul>
-        <ul class="nav navbar-nav navbar-right collapse navbar-collapse">
-            <li ><a href="#"><img class="twiter" src="img/twitter.png"></a></li>
-            <li><a href="#"><img class="face" src="img/facebook.png"></a></li>
-            <li><a href="#"><img class="link" src="img/linkedin.png"></a></li>
-        </ul>
-    </nav>
+
     <div id="header-img">
         <img src="img/header-img.png" class="img-responsive">
     </div>
-
-    <nav  style="margin-bottom: 5px; background-image: url(img/nav-bg.png);background-repeat: repeat-x;height: 80px; width: 100%;" class="navbar navbar-default">
+    <nav  class="navbar navbar-default">
         <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
-                <button  type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
                     <span class="sr-only">Toggle navigation</span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
+                <a class="navbar-brand" href="/"><img  id="logo" src="img/logo.png"/></a>
             </div>
 
-            <div class="collapse navbar-collapse" >
-                <ul id="linkovi" class="nav navbar-nav">
-                    <li><a href="#">Početna </a></li>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul id="linkovi"  class="nav navbar-nav">
+                    <li class="active1"><a href="#">Početna </a></li>
                     <li ><a href="#">O nama</a></li>
                     <li><a href="#">Vizija</a></li>
-                    <li><a href="#">Takmičari</a></li>
-                    <li><a href="#">Rekordi</a></li>
+                    <li  class=" dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Takmičenja <span class="caret"></span></a>
+                        <ul id="linkovi_dropdown" style="background-color: #003748" class=" dropdown-menu">
+                            <li><a href="#">Takmičari</a></li>
+                            <li><a href="#">Rekordi</a></li>
+                            <li><a href="#">Rezultati</a></li>
+                        </ul>
+                    </li>
+
                     <li><a href="#">Kalendar</a></li>
-                    <li><a href="#">Rezultati</a></li>
+
                     <li><a href="#">Galerija</a></li>
                     <li><a href="#">Norme</a></li>
                     <li><a href="/prijava">Prijava</a></li>
-                    <li><a href="/odjava">Odjava</a></li>
+                    {{--<li><a href="/odjava">Odjava</a></li>--}}
                 </ul>
+
+                <ul class="nav navbar-nav navbar-right" >
+                    <li style="display: inline;" ><a href="#"><img class="twiter" src="img/twitter.png"></a></li>
+                    <li style="display: inline;"><a href="#"><img class="face" src="img/facebook.png"></a></li>
+                    <li style="display: inline;"><a href="#"><img class="link" src="img/linkedin.png"></a></li>
+                </ul>
+
             </div><!-- /.navbar-collapse -->
         </div><!-- /.container-fluid -->
     </nav>
+
     @yield('body')
 </body>
 </html>
