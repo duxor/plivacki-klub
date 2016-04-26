1ae007c3ff186824130de14fbecc4413781faf47
Saša Jović <jovicsasajovic@gmail.com>
2016-04-26 21:12:15 +0200

    0.23

--- resources/views/admin/dodaj-norme.blade.php
+++ resources/views/admin/dodaj-norme.blade.php
@@ -3,4 +3,32 @@
 @extends('admin.master')
 @section('container')
 norme
+	{!!Html::style('/datepicker/datetimepicker.css')!!}
+    {!!Html::script('/datepicker/moment.js')!!}
+    {!!Html::script('/datepicker/datetimepicker.js')!!}
+    {!!Html::style('/trumbowyg/ui/trumbowyg.min.css')!!}
+    {!!Html::script('/trumbowyg/trumbowyg.min.js')!!}
+    @if(isset($uspesnoDodavanje))
+        <div class="alert alert-success alert-autocloseable-success">
+            <h2>{{$uspesnoDodavanje}}</h2>
+        </div>
+    @endif
+     @if(count($errors)>0)
+        <div class="alert alert-danger alert-autocloseable-success">
+            <ul>
+                @foreach($errors->all() as $error)
+                    <li>{{$error}}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    <script>
+        $(".alert-autocloseable-success").fadeTo(5000, 500).slideUp(500, function(){
+            $(".alert-autocloseable-success").alert('close');
+        });
+    </script>
+
+
+
+
 @endsection
